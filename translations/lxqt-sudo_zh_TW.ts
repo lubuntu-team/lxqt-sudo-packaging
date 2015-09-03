dd07fa57f56d0c5a91514dec19cce0a0d5a6bf92
Alf Gaida <agaida@siduction.org>
2015-09-04 01:31:13 +0200

    Cherry-picking upstream version 0.9.0+21050831.

--- translations/lxqt-sudo_zh_TW.ts
+++ translations/lxqt-sudo_zh_TW.ts
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="de">
+<TS version="2.0">
 <context>
     <name>PasswordDialog</name>
     <message>
@@ -13,25 +13,25 @@
         <location filename="../passworddialog.ui" line="77"/>
         <location filename="../build/ui_passworddialog.h" line="134"/>
         <source>Command:</source>
-        <translation>Befehl:</translation>
+        <translation>指令：</translation>
     </message>
     <message>
         <location filename="../passworddialog.ui" line="84"/>
         <location filename="../build/ui_passworddialog.h" line="135"/>
         <source>Password:</source>
-        <translation>Kennwort:</translation>
+        <translation>密碼：</translation>
     </message>
     <message>
         <location filename="../passworddialog.cpp" line="43"/>
         <source>&lt;b&gt;%1&lt;/b&gt; needs administrative privileges.
 Please enter your password.</source>
-        <translation>&lt;b&gt;%1&lt;/b&gt; benötigt root-Rechte.
-Bitte Kennwort eingeben.</translation>
+        <translation>&lt;b&gt;%1&lt;/b&gt; 需要管理員權限
+請輸入您的密碼 </translation>
     </message>
     <message>
         <location filename="../passworddialog.cpp" line="54"/>
         <source>Attempt #%1</source>
-        <translation>Versuch #%1</translation>
+        <translation>嘗試 %1</translation>
     </message>
 </context>
 <context>
@@ -47,13 +47,13 @@ Arguments:
   arguments      Optional arguments for command.
 
 </source>
-        <translation>Verwendung: %1 command [arguments...]
+<translation>用法：%1 指令 [選項...]
 
-graphische Benutzeroberfläche für %2
+%2 的圖型前端
 
-Argumente:
-  command        auszuführender Befehl
-  arguments      Argumente zum auszuführenden Befehl
+選項：
+  指令           要執行的指令
+  選項           給指令的選項
 
 </translation>
     </message>
@@ -61,19 +61,18 @@ Argumente:
         <location filename="../main.cpp" line="69"/>
         <source>%1 version %2
 </source>
-        <translation>%1 Version %2
-</translation>
+        <translation>%1 版本 %s</translation>
     </message>
     <message>
         <location filename="../main.cpp" line="80"/>
         <source>%1: no command to run provided!</source>
-        <translation>%1: kein auszuführender Befehl angegeben!</translation>
+        <translation>%1：沒有可執行的指令</translation>
     </message>
     <message>
         <location filename="../main.cpp" line="131"/>
         <source>Child &apos;%1&apos; process failed!
 %2</source>
-        <translation>nachgeordneter Prozess &apos;%1&apos; ist fehlgeschlagen!
+        <translation>子執行序&apos;%1&apos;執行失敗！
 %2</translation>
     </message>
 </context>
